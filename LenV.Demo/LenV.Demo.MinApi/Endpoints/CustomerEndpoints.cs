5895c6cf96e09f40ed27555bb8fbff50f8f10e71
Jochen Schoonjans <jochenschoonjans@hotmail.com>
2022-05-24 15:17:07 +0200

    refactor: endpoints program.cs

---
+++ LenV.Demo/LenV.Demo.MinApi/Endpoints/CustomerEndpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace LenV.Demo.MinApi.Endpoints
+{
+    public static class CustomerEndpoints
+    {
+        public static WebApplication UseCustomerEndpoints(this WebApplication app)
+        {
+            app.MapGet("/customers", async (ICustomerService customerService) =>
+            {
+                return await customerService.GetAllAsync();
+            })
+            .WithName("GetCustomers");
+
+            return app;
+        }
+    }
+}
